6023a75e4d5c75c2cc4a4cd1c6e0d3706b2c25c8
Alberto Sabater <asabater.work@gmail.com>
2019-02-28 15:46:24 +0100

    bad trainings and models removing function added. Print lr on callback added. tran/val annotations added

--- train.py
+++ train.py
@@ -34,16 +34,26 @@ freeze_body = 1     # freeze_body = 1 -> freeze prertained layers
 input_shape = (416,416) # multiple of 32, hw
 val_split = 0.1
 batch_size_frozen = 32          # 32
-batch_size_unfrozen = 32        # note that more GPU memory is required after unfreezing the body
-frozen_epochs = 1               # 50
+batch_size_unfrozen = 8        # note that more GPU memory is required after unfreezing the body
+frozen_epochs = 10               # 50
+
+
+
+title = 'Remove null trainings'; print('{} {} {}'.format('='*print_indnt, title, '='*(print_line-2 - len(title)-print_indnt)))
+train_utils.remove_null_trainings(path_results, dataset_name)
+print('='*print_line)
+
 
 
 anchors_path = 'base_models/yolo_anchors.txt'
+path_dataset = ''
 
 # Get dataset annotations, classes and anchors
 if dataset_name == 'adl':
-    path_dataset = '/home/asabater/projects/ADL_dataset/ADL_frames/'
-    path_annotations = '/home/asabater/projects/ADL_dataset/annotations_adl.txt'
+#    path_dataset = '/mnt/hdd/datasets/adl_dataset/ADL_frames/'
+    path_dataset = '/home/asabater/projects/ADL_dataset/ADL_frames_416/'
+    path_annotations = ['/home/asabater/projects/ADL_dataset/annotations_adl_train.txt',
+                        '/home/asabater/projects/ADL_dataset/annotations_adl_val.txt']
     path_classes = '/home/asabater/projects/ADL_dataset/adl_classes.txt'
 elif dataset_name == 'epic':
     path_annotations = '/home/asabater/projects/epic_dataset/annotations_epic_train.txt'
@@ -60,9 +70,25 @@ anchors = ktrain.get_anchors(anchors_path)
 
 
 
+# Train/Val split
+np.random.seed(10101)
+if type(path_annotations) == list:
+    with open(path_annotations[0]) as f: lines_train = [ path_dataset + l for l in f.readlines() ]
+    with open(path_annotations[1]) as f: lines_val = [ path_dataset + l for l in f.readlines() ]
+    num_train, num_val = len(lines_train), len(lines_val)
+else:
+    with open(path_annotations) as f: lines = [ path_dataset + l for l in f.readlines() ]
+    np.random.shuffle(lines)
+    num_val, num_train = int(len(lines)*val_split); len(lines) - num_val
+    lines_train = lines[:num_train]; lines_val = lines[num_train:]
+np.random.shuffle(lines_train), np.random.shuffle(lines_val)
+np.random.seed(None)
+
+
+
 title = 'Create and get model folders'; print('{} {} {}'.format('='*print_indnt, title, '='*(print_line-2 - len(title)-print_indnt)))
 # Create and get model folders
-path_log, path_model = train_utils.create_model_folder(path_results, dataset_name)
+path_model = train_utils.create_model_folder(path_results, dataset_name)
 print(path_model)
 print('='*print_line)
 
@@ -83,20 +109,13 @@ print('='*print_line)
 # Train callbacks
 logging = TensorBoard(log_dir = path_model)
 checkpoint = ModelCheckpoint(path_model + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-                             monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+                             monitor='val_loss', 
+                             save_weights_only=True, 
+                             save_best_only=True, 
+                             period=2)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
 early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
     
-
-
-# Train/Val split
-with open(path_annotations) as f:
-    lines = f.readlines()
-np.random.seed(10101)
-np.random.shuffle(lines)
-np.random.seed(None)
-num_val = int(len(lines)*val_split)
-num_train = len(lines) - num_val
 
 
 
@@ -112,6 +131,7 @@ train_params = {
                 'anchors_path': anchors_path,
                 'path_annotations': path_annotations,
                 'path_classes': path_classes,
+                'path_dataset': path_dataset,
                 'num_val': num_val,
                 'num_train': num_train,
                 }
@@ -133,15 +153,17 @@ print('='*print_line)
 
 title = 'Train first stage'; print('{} {} {}'.format('='*print_indnt, title, '='*(print_line-2 - len(title)-print_indnt)))
 if True:
-    model.compile(optimizer=Adam(lr=1e-3), loss={
-        # use custom yolo_loss Lambda layer.
-        'yolo_loss': lambda y_true, y_pred: y_pred})
+    optimizer = Adam(lr=1e-3)
+    model.compile(optimizer = optimizer,
+                  loss = {'yolo_loss': lambda y_true, y_pred: y_pred},        # use custom yolo_loss Lambda layer.
+                  metrics = [train_utils.get_lr_metric(optimizer)]
+                  )
 
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size_frozen))
     model.fit_generator(
-            ktrain.data_generator_wrapper(lines[:num_train], batch_size_frozen, input_shape, anchors, num_classes),
+            ktrain.data_generator_wrapper(lines_train, batch_size_frozen, input_shape, anchors, num_classes),
             steps_per_epoch = max(1, num_train//batch_size_frozen),
-            validation_data = ktrain.data_generator_wrapper(lines[num_train:], batch_size_frozen, input_shape, anchors, num_classes),
+            validation_data = ktrain.data_generator_wrapper(lines_val, batch_size_frozen, input_shape, anchors, num_classes),
             validation_steps = max(1, num_val//batch_size_frozen),
             epochs = frozen_epochs,
             initial_epoch = 0,
@@ -160,20 +182,33 @@ title = 'Train second stage'; print('{} {} {}'.format('='*print_indnt, title, '=
 if True:
     for i in range(len(model.layers)):
         model.layers[i].trainable = True
-    model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
     print('Unfreeze all of the layers.')
+
+    optimizer = Adam(lr=1e-4)
+    model.compile(optimizer = optimizer,
+                  loss = {'yolo_loss': lambda y_true, y_pred: y_pred},        # use custom yolo_loss Lambda layer.
+                  metrics = train_utils.get_lr_metric(optimizer)
+                  )
+
 
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size_unfrozen))
     model.fit_generator(
-        ktrain.data_generator_wrapper(lines[:num_train], batch_size_unfrozen, input_shape, anchors, num_classes),
+        ktrain.data_generator_wrapper(lines_train, batch_size_unfrozen, input_shape, anchors, num_classes),
         steps_per_epoch = max(1, num_train//batch_size_unfrozen),
-        validation_data = ktrain.data_generator_wrapper(lines[num_train:], batch_size_unfrozen, input_shape, anchors, num_classes),
+        validation_data = ktrain.data_generator_wrapper(lines_val, batch_size_unfrozen, input_shape, anchors, num_classes),
         validation_steps = max(1, num_val//batch_size_unfrozen),
         epochs = 500,
-        initial_epoch = 50,
+        initial_epoch = frozen_epochs,
         callbacks = [logging, checkpoint, reduce_lr, early_stopping])
     model.save_weights(path_model + 'trained_weights_final.h5')
 print('='*print_line)
     
     
+# %%
 
+train_utils.remove_worst_weights(path_model)
+
+
+
+    
+    
--- train_utils.py
+++ train_utils.py
@@ -16,19 +16,21 @@ def create_folder_if_not_exists(folder_path):
 
 def create_model_folder(path_results, dataset_name):
     
-    path_log = path_results + dataset_name + '/logs/'
+#    path_log = path_results + dataset_name + '/logs/'
     path_model = path_results + dataset_name + '/models/'
     
     # Create logs and model main folder
-    create_folder_if_not_exists(path_log)
+#    create_folder_if_not_exists(path_log)
     create_folder_if_not_exists(path_model)
 
     model_name = get_model_name(path_results + dataset_name + '/models/')
-    path_log += model_name; path_model += model_name
-    create_folder_if_not_exists(path_log)
+#    path_log += model_name; 
+    path_model += model_name
+#    create_folder_if_not_exists(path_log)
     create_folder_if_not_exists(path_model)
     
-    return path_log, path_model
+#    return path_log, path_model
+    return path_model
 
 
 def get_model_name(model_dir):
@@ -36,9 +38,47 @@ def get_model_name(model_dir):
     return '{}_model_{}/'.format(datetime.datetime.today().strftime('%m%d_%H%M'), folder_num)
 
 
-# TODO
-def remove_empty_folders(path_results, dataset_name):
-    pass
+# Remove folders whose training has not finished the frozen stage
+def remove_null_trainings(path_results, dataset_name):
+    folder_models = path_results + dataset_name + '/models/'
+    models = os.listdir(folder_models)
+    
+    models_to_remove = []
+    for model in models:
+        if 'trained_weights_stage_1.h5' not in os.listdir(folder_models + model):
+            models_to_remove.append(model)
+            
+    for mtr in models_to_remove:
+        for f in os.listdir(folder_models + mtr): 
+            print('remove', folder_models + mtr + '/' + f)
+            os.remove(folder_models + mtr + '/' + f)
+        os.rmdir(folder_models + mtr)
+        
+    
+    count = 0
+    for i, model in enumerate(os.listdir(folder_models)):
+        if int(model.split('_')[-1]) != count:
+            print('rename', model)
+            os.rename(folder_models + model, folder_models + '_'.join(model.split('_')[:-1]) + '_' + str(count))
+        count += 1
+
+
+# Remove the worst weight os a model
+def remove_worst_weights(path_model):
+    files_model = [ f for f in os.listdir(path_model) if f.endswith('.h5') and not f.startswith('trained_weights') ]
+    files_model = sorted(files_model, key=lambda x: [ int(i[2:]) for i in x[:-3].split('-') if i.startswith('ep') ][0], reverse=True)
+    
+    for fm in files_model[1:]:
+        os.remove(path_model, fm)
+
+
+# Print learning_rate on each epoch
+def get_lr_metric(optimizer):
+    def lr(y_true, y_pred):
+        return optimizer.lr
+    return lr
+
+
 
 
 
